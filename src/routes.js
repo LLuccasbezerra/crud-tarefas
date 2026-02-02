d38fa28f362a02e217f6462408f3e14d0530f01a
luccas <luccasifes@gmail.com>
2026-02-01 22:15:12 -0300

    rotas

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { randomUUID } from 'node:crypto'
 import { Database } from './database.js'
 import { buildRoutePath } from './utils/build-route-path.js'
+import path from 'node:path'
 
 const database = new Database()
 
@@ -10,14 +11,27 @@ export const routes = [
     method: 'GET',
     path: buildRoutePath('/tasks'),
     handler: (req,res) => {
-        const { search } = req.query
-    
-      const tasks = database.select('tasks', search? {
-        title : search, 
-        description : search
-      }: null)
-      
-      return res.end(JSON.strigfy(tasks))
+        const { title,description } = req.query
+      let filters = null
+
+      if (title || description) {
+        filters = {}
+
+        if (title) {
+          filters.title = title
+        }
+
+        if (description) {
+          filters.description = description
+        }
+      }
+      // const tasks = database.select('tasks', search? {
+      //   title : search, 
+      //   description : search
+      // }: null)
+      const tasks = database.select('tasks', filters)
+
+      return res.end(JSON.stringify(tasks))
     }
  },
  {
@@ -25,15 +39,17 @@ export const routes = [
     path: buildRoutePath('/tasks'),
     handler: (req,res) => {
         const {title,description} = req.body 
-
+    const dateNow = new Date().toLocaleString("pt-BR", {
+      timeZone: "America/Sao_Paulo"
+    });
         const task = {
 
           id: randomUUID(), 
           title,
           description,
-          created_at: Date.now(),
-          updated_at: Date.now(),
-          completed_at: Date.now()
+          created_at: dateNow,
+          updated_at: dateNow,
+          completed_at: null
 
         }
         
@@ -42,5 +58,75 @@ export const routes = [
         return res.writeHead(201).end()
 
     }
+ },
+ {
+    method: 'PUT',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req,res) => {
+        const { id } = req.params
+        const { title,description } = req.body
+        const dateNow = new Date().toLocaleString("pt-BR", {
+          timeZone: "America/Sao_Paulo"
+        });
+        if(database.select('tasks',{id}).length === 0){
+          return res.writeHead(404).end(
+            JSON.stringify({message: 'Task not found!'})
+          )
+        } 
+        else {
+
+          database.update('tasks', id, {
+              title,
+              description,
+              updated_at: dateNow
+            })
+      }
+        return res.writeHead(204).end()
+    }
+ },
+ {
+    method: 'PATCH',
+    path: buildRoutePath('/tasks/:id/complete'),
+    handler: (req,res) => {
+        const { id } = req.params
+        const dateNow = new Date().toLocaleString("pt-BR", {
+          timeZone: "America/Sao_Paulo"
+        });
+        if(database.select('tasks',{id}).length === 0){
+          return res.writeHead(404).end(
+            JSON.stringify({message: 'Task not found!'})
+          )
+        }
+        else {
+
+          const task = database.select('tasks',{id})[0]
+          const isTaskCompleted = !!task.completed_at
+
+          database.update('tasks', id, {
+              ...task,
+              completed_at: isTaskCompleted ? null : dateNow
+            })
+        }
+        return res.writeHead(204).end()
+    }
+ },
+ {
+    method: 'DELETE',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req,res) => {
+        const { id } = req.params
+
+        if(database.select('tasks',{id}).length === 0){
+          return res.writeHead(404).end(
+            JSON.stringify({message: 'Task not found!'})
+          )
+        } else {
+
+           database.delete('tasks', id)
+        }
+
+        return res.writeHead(204).end()
+    }
+    
  }
 ]
